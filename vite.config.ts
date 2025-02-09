9cdf3b43b04e9466bbf29f024dc5362bb0d08668
Harsh2227kumar <harsh2227kumar@gmail.com>
2025-02-09 20:08:13 +0530

    github

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Internship/",
+  base: '/Internship/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
